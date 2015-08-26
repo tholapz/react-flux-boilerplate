00493882619dbf56443fc28c41d45f1a8c1ddad4
Kamol Treewatchararat <kamol.t@vixlet.com>
2015-08-25 17:57:35 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var MyApp = require('./components/MyApp');
+var React = require('react');
+React.render(
+	<MyApp />,
+	document.getElementById('container')
+);
---
+++ js/components/MyApp.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var MyApp = React.createClass({
+	render: function () {
+		return (
+			<div className="myApp">
+			Hello, world!
+			</div>
+		);
+	}
+});
+
+
+module.exports = MyApp;
